33c04b0094a4539c2f07a794f93c823ba3ca8eda
Elizabeth Oliver and Nathaniel Meyer <nathaniel@>
2017-02-14 11:40:11 -0800

    Populate html, add functions to show definitions, some styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,65 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
   });
+  $("h2#javascript").click(function() {
+    $("#javascriptParagraph").toggle();
+  });
 
+  $("h2#operators").click(function() {
+    $("#operatorsParagraph").toggle();
+
+  });
+  $("h2#variables").click(function() {
+    $("#variablesParagraph").toggle();
+
+  });
+
+  $("h2#namingConventions").click(function() {
+      $("#conventionsParagraph").toggle();
+  });
+  $("h2#functions").click(function() {
+    $("#functionsParagraph").toggle();
+  });
+  $("h2#methods").click(function() {
+    $("#methodsParagraph").toggle();
+  });
+  $("h2#arguments").click(function() {
+    $("#argumentsParagraph").toggle();
+  });
+  $("h2#parameters").click(function() {
+    $("#parametersParagraph").toggle();
+  });
+  $("h2#return").click(function() {
+    $("#returnParagraph").toggle();
+  });
+  $("h2#chaining").click(function() {
+    $("#chainingParagraph").toggle();
+  });
+  $("h2#strings").click(function() {
+    $("#stringsParagraph").toggle();
+  });
+  $("h2#booleans").click(function() {
+    $("#booleansParagraph").toggle();
+  });
+  $("h2#undefined").click(function() {
+    $("#undefinedParagraph").toggle();
+  });
+  $("h2#nan").click(function() {
+    $("#nanParagraph").toggle();
+  });
+  $("h2#escape").click(function() {
+    $("#escapeParagraph").toggle();
+  });
+  $("h2#alert").click(function() {
+    $("#alertParagraph").toggle();
+  });
+  $("h2#comments").click(function() {
+    $("#commentsParagraph").toggle();
+  });
+  $("h2#jQuery").click(function() {
+    $("#jqueryParagraph").toggle();
+  });
+  $("h2#attributes").click(function() {
+    $("#attributesParagraph").toggle();
+  });
 });
